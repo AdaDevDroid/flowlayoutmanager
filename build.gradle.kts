e7ed73e6fb6addcc44344b106b1a754e16beef73
taeKrits <kritsada@ada-soft.com>
2025-01-27 14:33:35 +0700

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,23 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.AdaDevDroid" // GitHub Username
+            artifactId = "flowlayoutmanager"        // ชื่อ Library
+            version = "1.0.0"              // เวอร์ชันของ Library
+
+            // ระบุไฟล์ .aar ที่ต้องการเผยแพร่
+            artifact("libs/flowlayoutmanager-1.0.2.aar") {
+                extension = "aar"          // ระบุชนิดไฟล์
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
 }
